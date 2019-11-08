a2dda6748c43b008be0d498e89d40af78b54d952
MOSH_IS_A_LEGEND <mosleh.edu@gmail.com>
2019-11-08 10:54:48 +0100

    Initial commit

---
+++ src/com/company/Tokenizer.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Tokenizer implements ITokenizer {
+    @Override
+    public void open(String fileName) throws IOException, TokenizerException {
+
+    }
+
+    @Override
+    public Lexeme current() {
+        return null;
+    }
+
+    @Override
+    public void moveNext() throws IOException, TokenizerException {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
